8233497cf6f541bca004537b933a0fa6e16e986f
Shawn Wilson <wilson.ohyeah@gmail.com>
2025-07-13 18:05:32 -0700

    Add new favicon. Uses svg for now, will add .ico later on

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxt/scripts",
+  ],
+  app: {
+    head: {
+      link: [
+        // SVG favicon for modern browsers
+        // TODO: Don't forget to eventaully add a favicon.ico file to the project
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      ],
+    },
+  },
+});
