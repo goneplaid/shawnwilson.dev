e5397cdf03bc38e6a22c8227bd6905ab2da0b1c6
Shawn Wilson <wilson.ohyeah@gmail.com>
2025-07-16 21:39:25 -0700

    Pre 4.0.0 Nuxt upgrade checkin of minor changes

--- app.vue
+++ app.vue
@@ -3,18 +3,24 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 </script>
 
 <template>
-  <div class="min-h-screen bg-background flex items-center justify-center">
-    <div class="text-center">
-      <Avatar class="w-32 h-32 mx-auto">
-        <AvatarImage src="/images/portrait.png" alt="Shawn Wilson" />
-        <AvatarFallback>SW</AvatarFallback>
-      </Avatar>
-      <h1 class="mt-6 text-4xl font-extrabold text-foreground font-display">
-        Shawn Wilson
-      </h1>
-      <p class="mt-2 text-lg text-muted-foreground font-medium font-sans">
-        Full-Stack Product Engineer
-      </p>
+  <div class="min-h-screen p-12 h-full">
+    <div
+      class="max-w-7xl h-full m-auto border bg-background flex items-center justify-center"
+    >
+      <div class="text-center">
+        <Avatar class="w-32 h-32 mx-auto">
+          <AvatarImage src="/images/portrait.png" alt="Shawn Wilson" />
+          <AvatarFallback>SW</AvatarFallback>
+        </Avatar>
+        <h1
+          class="mt-6 text-4xl text-foreground font-display font-black tracking-tight"
+        >
+          Shawn Wilson
+        </h1>
+        <p class="mt-2 text-lg text-muted-foreground font-medium font-sans">
+          Full-Stack Product Engineer
+        </p>
+      </div>
     </div>
   </div>
 </template>
