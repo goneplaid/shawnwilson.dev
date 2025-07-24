c1e399aaac63ca992ba85550aeedae9b97bcb8bf
Shawn Wilson <wilson.ohyeah@gmail.com>
2025-07-23 22:14:03 -0700

    feat: dynamic positioning for drawer

--- app/components/ui/drawer/DrawerContent.vue
+++ app/components/ui/drawer/DrawerContent.vue
@@ -4,7 +4,9 @@ import type { HTMLAttributes } from "vue";
 import { useForwardPropsEmits } from "reka-ui";
 import { DrawerContent, DrawerPortal } from "vaul-vue";
 import { cn } from "@/lib/utils";
+import { computed, onMounted, nextTick } from "vue";
 import DrawerOverlay from "./DrawerOverlay.vue";
+import { useDrawerPositioning } from "@/composables/useDrawerPositioning";
 
 const props = defineProps<
   DialogContentProps & { class?: HTMLAttributes["class"] }
@@ -12,6 +14,23 @@ const props = defineProps<
 const emits = defineEmits<DialogContentEmits>();
 
 const forwarded = useForwardPropsEmits(props, emits);
+
+const { drawerLeftPosition, drawerWidth, calculateDrawerPosition } =
+  useDrawerPositioning();
+
+onMounted(() => {
+  nextTick(() => {
+    calculateDrawerPosition();
+  });
+});
+
+const rightDrawerStyles = computed(() => {
+  return {
+    left: `${drawerLeftPosition.value}px`,
+    width: `${drawerWidth.value}px`,
+    right: "auto",
+  };
+});
 </script>
 
 <template>
@@ -20,13 +39,16 @@ const forwarded = useForwardPropsEmits(props, emits);
     <DrawerContent
       data-slot="drawer-content"
       v-bind="forwarded"
+      :style="rightDrawerStyles"
       :class="
         cn(
           `group/drawer-content bg-background fixed z-50 flex h-auto flex-col`,
+          // Modified right drawer styles - removed width classes since we're using dynamic positioning
+          // for this use case.
+          `data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:border-l-2 data-[vaul-drawer-direction=right]:border-l-muted-foreground`,
+          // TODO: Handle these other directions, if needed (we probably won't even use them tbh)
           `data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg`,
           `data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg`,
-          `data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:sm:w-[696px]`,
-          `data-[vaul-drawer-direction=right]:border-l-2 data-[vaul-drawer-direction=right]:border-l-muted-foreground`,
           `data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:sm:max-w-sm`,
           props.class
         )
