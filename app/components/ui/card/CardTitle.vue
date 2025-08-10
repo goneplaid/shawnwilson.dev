d81eb9907506bb1fde4a454ec505aca6ea8b329b
Shawn Wilson <wilson.ohyeah@gmail.com>
2025-08-10 13:13:01 -0700

    feat: work sections

--- app/components/ui/card/CardTitle.vue
+++ app/components/ui/card/CardTitle.vue
@@ -1,26 +1,35 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from "vue"
-import { cn } from "@/lib/utils"
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
 
 interface Props {
-  class?: HTMLAttributes["class"]
-  headingLevel?: 1 | 2 | 3 | 4 | 5 | 6
+  class?: HTMLAttributes["class"];
+  headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   headingLevel: 3,
-})
+});
 
-const headingTag = computed(() => `h${props.headingLevel}` as const)
+const headingTag = computed(() => `h${props.headingLevel}` as const);
 </script>
 
 <template>
   <component
     :is="headingTag"
-    :class="
-      cn('font-semibold leading-none tracking-tight', props.class)
-    "
+    :class="cn('font-semibold leading-none tracking-tight', props.class)"
   >
     <slot />
   </component>
 </template>
+
+<style lang="css" scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply mb-0;
+}
+</style>
---
+++ app/components/work/ContentColumn.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h4>{{ title }}</h4>
+    <ul>
+      <li>
+        <p>Some content</p>
+      </li>
+      <li>
+        <p>Some content</p>
+      </li>
+      <li>
+        <p>Some content</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  title: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+section {
+  @apply flex flex-col mb-4 md:mb-0;
+}
+
+h4 {
+  @apply pb-4 border-b-2 border-muted-foreground;
+}
+
+ul {
+  @apply flex flex-col gap-8 pt-8;
+}
+</style>
