779e4b246c603228b14bcbd7b080f2e01cfe4276
Shawn Wilson <wilson.ohyeah@gmail.com>
2025-08-10 13:13:01 -0700

    feat: work sections

--- app/pages/work.vue
+++ app/pages/work.vue
@@ -1,13 +1,18 @@
 <template>
-    <main>
-        <h1>Work</h1>
-    </main>
-    
+  <main>
+    <h1>Work</h1>
+    <ContentGrid>
+      <ContentColumn title="Relevant Experience" />
+      <ContentColumn title="Selected Projects" />
+    </ContentGrid>
+  </main>
 </template>
 
 <script setup lang="ts">
+import ContentGrid from "~/components/ContentGrid.vue";
+import ContentColumn from "~/components/work/ContentColumn.vue";
+
 definePageMeta({
   layout: "default-layout",
 });
 </script>
-
